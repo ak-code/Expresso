1182b17a33f861a5e8d530695754158eececa447
akahn-github <ak@tinymanager.com>
2018-01-21 21:49:45 -0600

    Create Employee, Timesheet, Menu and MenuItem Tables

---
+++ server.js
@@ -0,0 +1,19 @@
+const cors = require('cors')
+const errorhandler = require('errorhandler')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const express = require('express')
+const app = express()
+
+app.use(cors())
+app.use(errorhandler())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
+})
+
+module.exports = app
