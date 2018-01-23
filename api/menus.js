dbf8cff64922e7a5414ef970b715e01304f4df01
akahn-github <ak@tinymanager.com>
2018-01-22 22:09:58 -0600

    Create Menu Routes

---
+++ api/menus.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const menuRouter = express.Router()
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+
+menuRouter.param('menuId', (req, res, next, menuId) => {
+  db.get('SELECT * FROM Menu WHERE Menu.id = $menuId', {$menuId: menuId}, (error, menu) => {
+    if (error) {
+      next(error)
+    } else if (menu) {
+      req.menu = menu
+      next()
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
+
+menuRouter.get('/', (req, res, next) => {
+  db.all(`SELECT * FROM Menu`, (error, menus) => {
+    if (error) {
+      next(error)
+    } else {
+      res.status(200).json({menus: menus})
+    }
+  })
+})
+
+menuRouter.get('/:menuId', (req, res, next) => {
+  res.status(200).json({menu: req.menu})
+})
+
+menuRouter.post('/', (req, res, next) => {
+  const title = req.body.menu.title
+
+  if (!title) {
+    return res.sendStatus(400)
+  }
+
+  db.run(`INSERT INTO Menu (title) VALUES ($title)`, { $title: title }, function(error) {
+    if (error) {
+      next(error)
+    } else {
+      db.get(`SELECT * FROM Menu WHERE Menu.id = ${this.lastID}`, (error, menu) => {
+        res.status(201).json({menu: menu});
+      })
+    }
+  })
+})
+
+menuRouter.put('/:menuId', (req, res, next) => {
+  const title = req.body.menu.title
+
+  if (!title) {
+    return res.sendStatus(400)
+  }
+
+  db.run(`UPDATE Menu SET title = $title WHERE Menu.id = $menuId`,
+    {
+      $title: title,
+      $menuId: req.params.menuId
+    }, function(error) {
+    if (error) {
+      next(error)
+    } else {
+      db.get(`SELECT * FROM Menu WHERE Menu.id = $menuId`, {$menuId: req.params.menuId}, (error, menu) => {
+        res.status(200).json({menu: menu});
+      })
+    }
+  })
+})
+
+module.exports = menuRouter
